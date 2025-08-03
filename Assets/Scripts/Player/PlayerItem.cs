dc1e6a89d87c0efcca7bfaa1be4d9c87713edf58
MekhelPrat <c14220141@john.petra.ac.id>
2025-08-03 20:17:00 +0700

    pppp

---
+++ Assets/Scripts/Player/PlayerItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItem : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
